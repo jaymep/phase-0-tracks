c18507d1ca3e2b6e366137f98504c15cdcaa7fc3
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-03 12:40:13 -0700

    Add allergy loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,9 @@
 #At the beginning of your program, ask the user how many employees will be processed, then begin the survey process for the first employee. After you print the result for that employee, start the survey over again, and repeat until all the employees have been processed.
+puts('How many employees today?')
+n = gets.chomp
+n = n.to_i
+
+# Loop survey n times then final statement
 
 puts("What is your name?")
 name = gets.chomp
@@ -20,6 +25,7 @@ if garlic = 'Y'
   garlic =  TRUE
 else
   garlic = FALSE
+end
 
 puts("Would you like to enroll in the company’s health insurance? (Y/N)")
 insurance = gets.chomp
@@ -27,6 +33,7 @@ if insurance = 'Y'
   insurance =  TRUE
 else
   insurance = FALSE
+end
 
 
 # Match the following conditions in the order they're listed. Your program should base its result on the latest condition matched, not the first condition matched. In other words, it's not a "only one condition will apply" sort of scenario. This may mean repeatedly updating a variable as each condition is checked, so that the variable always contains the most precise result.
@@ -50,16 +57,30 @@ if name == 'Drake Cula' || name == 'Tu Fang'
 # Otherwise, print “Results inconclusive.”
 else
   status = 'Results inconclusive'
+end
 
-# Print the result at the end of the survey.
-print('Employee Status ' + status)
 
 # In addition to the other questions in the survey, use a loop to ask the employee to name any allergies, one at a time. The employee can type “done” when finished.
-puts('What are you allergic to? (Enter 'done' when finished)')
+puts('What are you allergic to? (Enter one allergy at a time, then 'done' when finished)')
 allergy = gets.chomp
 until allergy = 'done'
+  puts('What are you allergic to? (Enter one allergy at a time, then 'done' when finished)')
+  allergy = gets.chomp
+  if allergy = 'sunshine'
+    status = 'Probably a vampire'
+  end
+end
 
 
 # As long as the allergy is not “sunshine,” continue the loop for as long as is needed. If at any point the employee lists “sunshine” as an allergy, skip directly to the result of “Probably a vampire.”
 
+
+
+
+# Print the result at the end of the survey.
+print('Employee Status ' + status)
+
+# End employee survey loop here
+
 # Add a line at the very end of your program that prints, "Actually, never mind! What do these questions have to do with anything? Let's all be friends." right before it exits. Ahhh, much better.
+print("Actually, never mind! What do these questions have to do with anything? Let's all be friends.")
