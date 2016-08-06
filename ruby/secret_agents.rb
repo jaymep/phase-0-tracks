7c19fe1248ff584060be639e3be5d9c75933e63b
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-05 17:37:42 -0700

    Add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -10,8 +10,29 @@ def encrypt(str)
      message << str[counter].next
     counter += 1
   end
-  p message
+    p message
 end
 
 #Test string
 encrypt("hello")
+
+# Decrypt: take each letter in string and go back 1 letter in alphabet
+# Find each letter's position in alphabet and subtract 1 to get new letter
+# Create new string from results
+
+def decrypt(msg)
+  dlen = msg.length
+  pos = 0
+  result = ""
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  while pos < dlen
+    secret = alphabet.index(msg[pos])
+    agent = secret - 1
+    result << alphabet[agent]
+    pos += 1 
+  end
+  p result
+end
+
+#Test string
+decrypt("ifmmp")
