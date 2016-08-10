729e71233597e355cdc700b70b2a08e984579911
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-09 20:28:06 -0700

    Add build and append array methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,15 @@
+#Methods
+def build_array(a, b, c)
+  new_array = [a,b,c]
+end
+
+def add_to_array(array, b)
+  array << b
+end
+  
+
+
+
 jayme = []
 p jayme
 
@@ -24,3 +36,8 @@ p aidan
 
 pairs = jayme | aidan
 p pairs
+
+p build_array("monday", "tuesday", "wednesday")
+p add_to_array(["apples"], "bannanas")
+p add_to_array([1,"two",3,"four",5,"six",7],"eight")
+p add_to_array(["red","orange", "yellow", "green", "blue", "indigo"], "violet")
