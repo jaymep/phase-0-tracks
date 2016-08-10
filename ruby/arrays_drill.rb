54dfeb569963aa21ad6cd1ce33748efdffb9309f
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-09 19:59:15 -0700

    Add include if statement and combine arrays

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,8 +1,26 @@
 jayme = []
 p jayme
 
-jayme = ["shirt", "pants", "shoes", "hat", "glasses"]
+jayme.push("shirt", "pants", "shoes", "hat", "glasses")
 p jayme
 
 jayme.delete_at(2)
 p jayme
+
+jayme.insert(2, "flip flops")
+p jayme
+
+jayme.shift
+p jayme
+
+if jayme.include? "glasses"
+  p "Jayme has glasses!"
+else
+  p "Jayme does not have glasses."
+end
+
+aidan = ["socks", "jacket", "belt"]
+p aidan
+
+pairs = jayme | aidan
+p pairs
