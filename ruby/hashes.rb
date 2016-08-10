01568d01f49b97dfbbc30fe10939c3ddd7552595
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-09 17:09:48 -0700

    Add pseudocode for program

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+# Write a program that will allow an interior designer to enter the
+# details of a given client: the client's name, age, number of
+# children, decor theme, and so on (you can choose your own as long as
+# it's a good mix of string, integer, and boolean data)
+
+# Create new hash
+# Ask user to input data for each key
+# Convert input to appropriate data type
+# Add key/value pair to hash
+# Print all key/value pairs
+# 
+# Ask user if there are any changes
+#   If "none", "" or " " entered
+#     Print all key/value pairs
+#   elsif key entered (#to_sym)
+#     Ask for new value
+#     Overwrite value for key
+#   end
+#
