7b45f3dd2518c1cc435ab2d6f49b16e647bee765
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-10 17:27:05 -0700

    Add .each for hash and array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,6 +7,11 @@ end
 
 greetings { puts "Ciao!"}
 
+###
+
 cheese = [ "cheddar", "pepperjack", "mozzarella", "gouda", "brie"]
-dishes = { penne: "pesto", rigatoni: "marinara", bowtie: "alfredo"; papardelle: "ragu", spaghetti: "meatballs" }
+dishes = { penne: "pesto", rigatoni: "marinara", bowtie: "alfredo", papardelle: "ragu", spaghetti: "meatballs" }
+
+cheese.each { |cheese| puts "I like eating #{cheese}." }
+dishes.each { |pasta, sauce| puts "#{pasta} goes great with #{sauce}." }
 
