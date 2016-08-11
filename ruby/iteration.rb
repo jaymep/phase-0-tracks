3ecd6782b8fd18eec0f3f1f3a76a90da344a0aae
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-10 17:22:17 -0700

    Add array and hash data

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,3 +7,6 @@ end
 
 greetings { puts "Ciao!"}
 
+cheese = [ "cheddar", "pepperjack", "mozzarella", "gouda", "brie"]
+dishes = { penne: "pesto", rigatoni: "marinara", bowtie: "alfredo"; papardelle: "ragu", spaghetti: "meatballs" }
+
