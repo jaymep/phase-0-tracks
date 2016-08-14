eb53af256f0f471748828f587195e9475d6cb1db
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-14 14:37:28 -0700

    Create nested TV data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,16 @@
+tv_programming = {
+  tv_series: { 
+    "tv_series_1" = > "1_episode_1",
+    "tv_series_2" = > "2_episode_1",
+    "tv_series_3" = > "3_episode_1"
+    },
+  newscasts: [
+    "morning_news",
+    "evening_news"],
+  advertisements: [
+    "cars",
+    "food",
+    "movies",
+    ]
+
+}
