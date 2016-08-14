71957731720108988b4a521177e5889d03b228e5
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-14 15:51:19 -0700

    Add code for nested structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,16 +1,21 @@
 tv_programming = {
   tv_series: { 
-    "tv_series_1" = > "1_episode_1",
-    "tv_series_2" = > "2_episode_1",
-    "tv_series_3" = > "3_episode_1"
+    "tv_series_1" => "1_episode_1",
+    "tv_series_2" => "2_episode_1",
+    "tv_series_3" => "3_episode_1"
     },
   newscasts: [
     "morning_news",
-    "evening_news"],
+    "evening_news",
+    "breaking_news"],
   advertisements: [
     "cars",
     "food",
     "movies",
-    ]
-
+    ],
+  sports: "football"
 }
+
+p tv_programming[:advertisements]
+
+tv_programming[:advertisements]
