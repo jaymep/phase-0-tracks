52988d9c5f5d811eac4612fc96aee8c23a30bef9
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-15 16:50:04 -0700

    Puppy roll over, age and high five methods added

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -10,13 +10,29 @@ class Puppy
     end
   end
 
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def dog_years(y) # Google says 1 dog year = 4 human years
+    dog_age = y / 4
+    puts "Puppy is #{dog_age} puppy-years old."
+  end
+  
+  def high_five(z)
+    z.times.each do puts "*high-fives*"
+    end
+  end
+  
 end
 
 # Driver code
 
-sparkles = Puppy.new
+# sparkles = Puppy.new
 
 # sparkles.fetch("ball")
 # sparkles.fetch("stick")
-
 # sparkles.speak(3)
+# sparkles.roll_over
+# sparkles.dog_years(13)
+# sparkles.high_five(5)
