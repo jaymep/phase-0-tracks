48a50eb6e0468c3b0db4cd539fd7cfdeee29f8d6
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-14 19:11:55 -0700

    Start pseudocode, vowel and consonant strings

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# Objectives:
+# Swapping the first and last name.
+# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+
+# Pseudocode:
+# Ask user for name, split on space
+# Reverse name array, join array with spaces
+# map next_vowel
+# map next_consonant
+# join into new string
+
+
+
+
+vowel = "aeioua"
+consonant = "bcdfghjklmnpqrstvwxyzb"
