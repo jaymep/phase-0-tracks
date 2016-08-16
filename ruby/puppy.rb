67a704d82068445d67253b056f33a8eda319aed1
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-16 15:44:15 -0700

    Design Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+SPECIES ------------------------
+Canis Cuteness
+
+CHARACTERISTICS ----------------
+Name: varies
+Color: varies
+Temperament: varies
+Weight: varies
+Age: varies
+Gender: M, F or N
+
+
+BEHAVIOR -----------------------
+Jump
+Pounce
+Retrieve
+Lick
+Chew
+Growl
+Yelp
+Wag tail
