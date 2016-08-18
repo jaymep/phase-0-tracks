040d85700214138da176c4997d9771f8c1abbe17
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-18 16:32:07 -0700

    Add more methods, need to test

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,11 +16,20 @@ class Santa
 
   def reindeer_ranking
     ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    # reindeer_ranking
   end
 
   def age
     @age = 0
+    # age
   end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    
 
 end
 
@@ -34,3 +43,19 @@ end
 # nick.speak
 # nick.eat_milk_and_cookies("snickerdoodle")
 
+# SantaCon Driver Code
+santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+p santas
