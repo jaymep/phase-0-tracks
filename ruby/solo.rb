57d808d02c679dfda508d7a5da590f3a5a4d473b
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-20 17:51:14 -0700

    Pseudoclass for Bellydancer

---
+++ ruby/solo.rb
@@ -0,0 +1,27 @@
+# Pseudocode
+
+# class Bellydancer
+
+# attributes
+#   name ==> string
+#   age ==> integer
+#   dance_style ==> string
+#   outfit_color ==> string
+#   routine_length ==> integer
+#   veil ==> boolean
+#   zills ==> boolean
+
+# methods
+#   hip_drop ==> number of times
+#   shimmy ==> area & duration
+#   turn ==> number of times
+#   entrance ==> entrance message
+#   exit ==> exit message
+
+class Bellydancer
+  attr_reader
+  attr_accessor
+
+  
+
+end
