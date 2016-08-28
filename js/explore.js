4312d3495b70550f6b62678d47e168cdfa3e8094
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-27 20:00:57 -0700

    Add pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// PSEUDOCODE
+// set original string as variable
+// set new string as variable (start with empty string)
+// set counter to start at end (index would be string length -1)
+// add letter at position of counter number to new string
+// decrease counter by 1 for each loop until counter is 0
+// print new string (keep print outside of loop)
+
