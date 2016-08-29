2cc055418f7ffc21d99f2fab4244754a6cc5bd53
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-29 08:26:15 -0700

    Add pseudocode for test data generator

--- js/algos.js
+++ js/algos.js
@@ -14,8 +14,17 @@
 //   otherwise repeat until match is found or no more keys to compare
 //   if no keys match, return false
 
-// Release 2: Generate Random Test Data
-
+Release 2: Generate Random Test Data
+  the given integer is set as a variable for array length
+  create an empty data array
+  loop as many times as array length
+    set alphabet as an array
+    generate a random number (1-10) for string length
+      loop as many times as string length
+        set string as empty array
+        generate random number (0-25) for letter index
+        add letter to string array
+      add string to data array
 
 // FUNCTION CODE
 // function maxLength(arr) {
@@ -28,19 +37,21 @@
 //   console.log(arr[maxLengthIndex]);
 // }
 
-function findMatch(obj1,obj2) {
-  for (var i = 0; i < Object.keys(obj1).length; i++) {
-    if (Object.keys(obj1)[i] === Object.keys(obj2)[i]) {
-      if (obj1[Object.keys(obj1)[i]] === obj2[Object.keys(obj2)[i]]) {
-       return true;
-      } 
-    }   
-  }  return false;
-}
-
-// function createData(set) {
-
+// function findMatch(obj1,obj2) {
+//   for (var i = 0; i < Object.keys(obj1).length; i++) {
+//     if (Object.keys(obj1)[i] === Object.keys(obj2)[i]) {
+//       if (obj1[Object.keys(obj1)[i]] === obj2[Object.keys(obj2)[i]]) {
+//        return true;
+//       } 
+//     }   
+//   }  return false;
 // }
+
+function createData(setLength) {
+  var dataSet = [];
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  
+}
 
 // DRIVER CODE
 // Release 0: Find the Longest Phrase
@@ -56,21 +67,20 @@ function findMatch(obj1,obj2) {
   // maxLength(["leonardo","raphael","donatello", "michaelangelo"]);
   // // return "michaelangelo"
 
-
 // Release 1: Find a Key-Value Match
-  var obj1 = {name: "Steven", age: 54}; 
-  var obj2 = {name: "Tamir", age: 54};
-  console.log(Object.keys(obj1).length); // object size
-  console.log(Object.keys(obj1)[0]); // key
-  console.log(obj1[Object.keys(obj1)[0]]); // value
-  console.log(findMatch(obj1,obj2));
+  // var obj1 = {name: "Steven", age: 54}; 
+  // var obj2 = {name: "Tamir", age: 54};
+  // console.log(Object.keys(obj1).length); // object size
+  // console.log(Object.keys(obj1)[0]); // key
+  // console.log(obj1[Object.keys(obj1)[0]]); // value
+  // console.log(findMatch(obj1,obj2));
 
-  var obj3 = {name: "Ryan", age: 24}; 
-  var obj4 = {named: "Ryan", age: 37};
-  console.log(findMatch(obj3,obj4));
+  // var obj3 = {name: "Ryan", age: 24}; 
+  // var obj4 = {named: "Ryan", age: 37};
+  // console.log(findMatch(obj3,obj4));
 
-  var obj5 = {color: "green", vehicle: "jeep", seats: 5}; 
-  var obj6 = {color: "yellow", vehicle: "submarine", seats: 8};
-  console.log(findMatch(obj5,obj6));
+  // var obj5 = {color: "green", vehicle: "jeep", seats: 5}; 
+  // var obj6 = {color: "yellow", vehicle: "submarine", seats: 8};
+  // console.log(findMatch(obj5,obj6));
 
 // Release 2: Generate Random Test Data
