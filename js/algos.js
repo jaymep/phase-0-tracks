601157f21484c8c95f44b5c5a47ff023196e1015
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-08-29 09:28:48 -0700

    Fix test data generator

--- js/algos.js
+++ js/algos.js
@@ -50,10 +50,11 @@ Release 2: Generate Random Test Data
 function createData(setLength) {
   var dataSet = [];
   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  // var alphabetIndex = Math.floor(Math.random() * (25 - 0) + 0); // do  not set, will not randomize if set
+  var alphabetIndex = Math.floor(Math.random() * (25 - 0) + 0);
   var stringLength = Math.floor(Math.random() * (10 - 1) + 1);
   var dataString = '';
   for (var i = 0; i < setLength; i ++) {
+    dataString = '';
     for (var j = 0; j < stringLength; j++) {
       dataString = dataString + alphabet[Math.floor(Math.random() * (25 - 0) + 0)];
       }    dataSet.push(dataString);
@@ -91,3 +92,4 @@ function createData(setLength) {
   // console.log(findMatch(obj5,obj6));
 
 // Release 2: Generate Random Test Data
+createData(3);
