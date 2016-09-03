5a5eec03b3d53ff64b90dc5bb99f553749914e27
Jayme Pangalangan <jaymepangalangan@gmail.com>
2016-09-03 15:59:31 -0700

    Create folder for 8.5 project, add notes and start ruby file

---
+++ databases/gig_tracker/gig_tracker.rb
@@ -0,0 +1,14 @@
+# GIG TRACKING
+# Create a database to track bellydance things
+# Create a table to track past and future gigs
+# Allow user to modify a gig entry
+# Allow user to create a gig entry
+# Allow user to print a sorted list of gigs
+
+# BONUS: (as in, if time allows)
+#   Allow user to generate report of performance fees earned
+#     in total
+#     per venue
+#     per specified period
+#   Add tables; complexity to database
+
